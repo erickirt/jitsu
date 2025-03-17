8c138b770535c77ce241dcce8bfec2147e718658
Ildar Nurislamov <absorbb@gmail.com>
2025-03-17 13:39:48 +0400

    console: use turbopack for dev deps upgrade

--- webapps/ee-api/next.config.js
+++ webapps/ee-api/next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    turbo: {
+      rules: {
+        '*.sql$': {
+          loaders: ['raw-loader'],
+          as: '*.js',
+        },
+      },
+    },
+  },
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config, opts) => {
