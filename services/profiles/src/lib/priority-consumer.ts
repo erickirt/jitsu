136a72ef9072832dd2e4899d70f2b7e8bde0ea6a
Ildar Nurislamov <absorbb@gmail.com>
2025-05-06 12:01:26 +0400

    profile builder tweaks

--- services/profiles/src/lib/priority-consumer.ts
+++ services/profiles/src/lib/priority-consumer.ts
@@ -2,14 +2,9 @@ import { ProfileBuilder } from "@jitsu/core-functions";
 import { kafkaAdmin, kafkaCredentials, kafkaSettings, topicName } from "./kafka";
 import PQueue from "p-queue";
 import { getLog, parseNumber } from "juava";
-import { KafkaJS, TopicPartitionOffsetSpec, OffsetSpec } from "@confluentinc/kafka-javascript";
-import { db, ProfileBuilderQueueInfo } from "./db";
-import { metricsInterval } from "../builder";
-import { promQueueProcessed, promQueueSize } from "./metrics";
+import { KafkaJS } from "@confluentinc/kafka-javascript";
 const concurrency = parseNumber(process.env.CONCURRENCY, 10);
 const instancesCount = parseNumber(process.env.INSTANCES_COUNT, 1);
-
-const log = getLog("priority-consumer");
 
 interface PriorityConsumer {
   start(): Promise<void>;
@@ -26,6 +21,8 @@ export function createPriorityConsumer(
   priorityLevels: number,
   profileTask: (profileId: string, priority: number) => () => Promise<void>
 ): PriorityConsumer {
+  const pbLongId = `${profileBuilder.workspaceId}-${profileBuilder.id}-v${profileBuilder.version}`;
+  const log = getLog(`pb-${pbLongId}`);
   let consumers: KafkaJS.Consumer[] = [];
   const rateLimitWindows: Record<ProfileId, RateLimitWindow> = {};
   const queue = new PQueue({ concurrency });
@@ -46,7 +43,6 @@ export function createPriorityConsumer(
   };
 
   const closeQueue = async () => {
-    log.atInfo().log("Closing queue...");
     await queue.onIdle();
   };
 
@@ -155,10 +151,13 @@ export function createPriorityConsumer(
     },
 
     async close(): Promise<void> {
+      log.atInfo().log("Closing consumers...");
       for (const consumer of consumers) {
         await consumer.disconnect();
       }
+      log.atInfo().log("Closing priority queue...");
       await closeQueue();
+      log.atInfo().log("Consumers closed");
     },
   };
 }
@@ -167,148 +166,3 @@ export type TopicsReport = Record<
   string,
   Record<number, { highOffset: number; offset: number; previousOffset?: number }>
 >;
-
-export async function reportQueueSize(
-  profileBuilder: ProfileBuilder,
-  priorityLevels: number,
-  previousOffsets?: TopicsReport
-): Promise<TopicsReport> {
-  log.atDebug().log(`Reporting queue size for ${profileBuilder.id}`);
-  const topics: TopicsReport = {};
-  for (let i = 0; i < priorityLevels; i++) {
-    const topic = topicName(profileBuilder.id, i);
-    topics[topic] = {};
-  }
-  const { promise, resolve, reject } = createDeferred();
-
-  kafkaAdmin.listConsumerGroupOffsets([{ groupId: "profile-builder-" + profileBuilder.id }], undefined, (e, data) => {
-    if (e) {
-      log
-        .atError()
-        .withCause(e)
-        .log(`Failed to describe topics ${JSON.stringify(topics)}`);
-      reject(e);
-      return;
-    }
-    for (const group of data) {
-      const partitions = group.partitions;
-      for (const partition of partitions) {
-        if (partition.error) {
-          log
-            .atError()
-            .log(`Failed to get partition ${partition.topic}:${partition.partition} offset: ${partition.error}`);
-          reject(partition.error);
-          return;
-        }
-        const topic = topics[partition.topic];
-        if (!topic) {
-          continue;
-        }
-        const previousOffset = previousOffsets?.[partition.topic]?.[partition.partition]?.offset;
-        const partitionInfo = topic[partition.partition];
-        if (!partitionInfo) {
-          topic[partition.partition] = { offset: partition.offset, highOffset: 0, previousOffset };
-        } else {
-          partitionInfo.offset = partition.offset;
-          partitionInfo.previousOffset = previousOffset;
-        }
-      }
-    }
-    resolve();
-  });
-  await promise;
-
-  const { promise: promise2, resolve: resolve2, reject: reject2 } = createDeferred();
-
-  kafkaAdmin.describeTopics(Object.keys(topics), undefined, (e, data) => {
-    if (e) {
-      log
-        .atError()
-        .withCause(e)
-        .log(`Failed to describe topics ${JSON.stringify(topics)}`);
-      reject2(e);
-      return;
-    }
-    const specs: TopicPartitionOffsetSpec[] = [];
-    for (const topic of data) {
-      if (topic.error) {
-        log.atError().log(`Failed to describe topic ${topic.name} : ${topic.error}`);
-        reject2(topic.error);
-        return;
-      }
-      const partitions = topic.partitions;
-      for (const partition of partitions) {
-        specs.push({
-          topic: topic.name,
-          partition: partition.partition,
-          offset: OffsetSpec.LATEST,
-        });
-      }
-    }
-    kafkaAdmin.listOffsets(specs, undefined, (e, data) => {
-      if (e) {
-        log
-          .atError()
-          .withCause(e)
-          .log(`Failed to list offsets ${JSON.stringify(topics)}`);
-        reject2(e);
-        return;
-      }
-      for (const partition of data) {
-        const topic = topics[partition.topic];
-        const partitionInfo = topic[partition.partition];
-        if (!partitionInfo) {
-          topic[partition.partition] = { highOffset: partition.offset, offset: 0 };
-        } else {
-          partitionInfo.highOffset = partition.offset;
-        }
-      }
-      resolve2();
-    });
-  });
-
-  await promise2;
-
-  const queues: ProfileBuilderQueueInfo["queues"] = {};
-  for (let i = 0; i < priorityLevels; i++) {
-    const name = topicName(profileBuilder.id, i);
-    const topic = topics[name];
-    const size = Object.values(topic).reduce((acc, partition) => {
-      if (partition.highOffset) {
-        return acc + (partition.highOffset - partition.offset);
-      }
-      return acc;
-    }, 0);
-    const processed = Object.values(topic).reduce((acc, partition) => {
-      if (partition.previousOffset) {
-        return acc + (partition.offset - partition.previousOffset);
-      }
-      return acc;
-    }, 0);
-    promQueueSize.labels({ builderId: profileBuilder.id, priority: i }).set(size);
-    promQueueProcessed.labels({ builderId: profileBuilder.id, priority: i }).inc(processed);
-    queues[i] = {
-      priority: i,
-      size,
-      processed,
-    };
-  }
-  log.atDebug().log(`Queue size: ${JSON.stringify(queues)}`);
-  await db.pgHelper().updateProfileBuilderQueuesInfo(profileBuilder.id, {
-    timestamp: new Date(),
-    intervalSec: metricsInterval / 1000,
-    queues,
-  });
-  return topics;
-}
-
-function createDeferred() {
-  let resolve, reject;
-
-  const promise = new Promise((res, rej) => {
-    resolve = res;
-    reject = rej;
-  });
-
-  return { promise, resolve, reject };
-}
