23d808a99212d664977490f9d645dcd6ffb95db9
Ildar Nurislamov <absorbb@gmail.com>
2025-09-10 16:18:33 +0400

    fix(console): bring back default deduplicate window value of 31 day

--- webapps/console/lib/schema/destinations.tsx
+++ webapps/console/lib/schema/destinations.tsx
@@ -154,7 +154,7 @@ export const BaseBulkerConnectionOptions = z
     mode: z.enum(["stream", "batch"]).default("batch"),
     primaryKey: z.string().default("message_id"),
     deduplicate: z.boolean().default(true),
-    deduplicateWindow: z.number().default(365),
+    deduplicateWindow: z.number().default(31),
     timestampColumn: z.string().default("timestamp"),
     dataLayout: z
       .enum(["segment", "jitsu-legacy", "segment-single-table", "passthrough"])
